8accb19907a38e965b8e096cff4984885e96c9b8
SENGJUN <49616211+tmdwns1521@users.noreply.github.com>
2024-04-02 01:10:48 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -3,7 +3,7 @@ import math
 def solution(n, a, b, answer = 0):
     answer += 1
     
-    if (((a > b) and a % 2 == 0) or (a < b) and b % 2 == 0) and abs(a - b) == 1:
+    if max(a, b) % 2 == 0 and abs(a - b) == 1:
         return answer
     
     a = math.ceil(a / 2)
