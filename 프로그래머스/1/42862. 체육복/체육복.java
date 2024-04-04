4f2fffb155156c302e2790b4bd340f6420a7e40b
SENGJUN <49616211+tmdwns1521@users.noreply.github.com>
2024-04-04 12:10:44 +0900

    [level 1] Title: 체육복, Time: 0.04 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int[] clothes = new int[n];
+        Arrays.fill(clothes, 1);
+        for (int l : lost) {
+            clothes[l - 1]--;
+        }
+        for (int r : reserve) {
+            clothes[r - 1]++;
+        }
+        for (int i = 0; i < n; i++) {
+            if (clothes[i] == 0) {
+                if (0 < i && clothes[i - 1] == 2) {
+                    clothes[i]++;
+                    clothes[i - 1]--;
+                } else if (i < n - 1 && clothes[i + 1] == 2) {
+                    clothes[i]++;
+                    clothes[i + 1]--;
+                }
+            }
+        }
+        int answer = 0;
+        for (int c : clothes) {
+            if (c > 0) answer++;
+        }
+        return answer;
+    }
+}
